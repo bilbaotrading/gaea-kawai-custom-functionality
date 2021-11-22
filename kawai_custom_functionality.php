526c33912145d74b7fa4561bdb0c0820ec332861
Gaby Molina Goigoux <gabymg@gabymg.es>
2021-11-22 20:34:23 +0100

    Validate tech support form

--- kawai_custom_functionality.php
+++ kawai_custom_functionality.php
@@ -37,3 +37,37 @@ function kawaispain_product_description_heading() {
 }
 
 add_filter('woocommerce_product_description_heading', 'kawaispain_product_description_heading');
+
+/******************************************************************************
+ *
+ * GRAVITY FORMS VALIDATION
+ *
+ *****************************************************************************/
+
+function is_serialnumber( $serial_number ) {
+    return false;
+}
+
+function kawaispain_validation_tech_support( $validation_result ) {
+    $form = $validation_result[ 'form' ];
+
+    foreach( $form['fields'] as &$field ) {
+    	if( strpos( $field->cssClass, 'validate-serialnumber' ) === true ) {
+		$serial_number = rgpost( "input_{$field['id']}" );
+		
+		$is_valid = is_serialnumber( $serial_number );
+
+		if( !$is_valid ) {
+			$validation_result[ 'is_valid' ] = false;
+
+			$field->failed_validation = true;
+			$field->validation_message = 'Has inroducido un número de serie incorrecto';
+		}	
+	}
+    }
+
+    $validation_result[ 'form' ] = $form;
+    return $validation_result;
+}
+
+add_filter( 'gform_validation_2', 'kawaispain_validation_tech_support' );
